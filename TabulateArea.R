4a059e11c267746eddb75f7f4811cd88a2bf5b46
damienjacques <damienjacques1989@gmail.com>
2017-07-11 09:47:15 +0200

    Add Raw Script of Tabulate Area Function

---
+++ TabulateArea.R
@@ -0,0 +1,9 @@
+TabulateArea <- function (x, z,digits = 0, na.rm = TRUE) { 
+  vals <- getValues(r) 
+  zones <- round(getValues(z), digits = digits)
+  rDT <- data.table(vals, z=zones)
+  Zstat <- count(rDT) 
+  colnames(Zstat)<-c("vals", "ID_grid","freq")
+  output <- dcast(data=Zstat, ID_grid ~vals , value.var="freq")
+  output[,2:ncol(output)] <- apply(output[,2:ncol(output)], 2, function(i) i/sum(i, na.rm=T))
+  return(output)}
