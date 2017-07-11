d9458a59839bb5b5f309658f4dd61817cf24d7b9
damienjacques <damienjacques1989@gmail.com>
2017-07-11 10:21:39 +0200

    ID_grid change in ID

--- TabulateArea.R
+++ TabulateArea.R
@@ -3,7 +3,9 @@ TabulateArea <- function (x, z,digits = 0, na.rm = TRUE) {
   zones <- round(getValues(z), digits = digits)
   rDT <- data.table(vals, z=zones)
   Zstat <- count(rDT) 
-  colnames(Zstat)<-c("vals", "ID_grid","freq")
-  output <- dcast(data=Zstat, ID_grid ~vals , value.var="freq")
+  colnames(Zstat)<-c("vals", "ID","freq")
+  output <- dcast(data=Zstat, ID ~ vals , value.var="freq")
   output[,2:ncol(output)] <- apply(output[,2:ncol(output)], 2, function(i) i/sum(i, na.rm=T))
   return(output)}
+
+
